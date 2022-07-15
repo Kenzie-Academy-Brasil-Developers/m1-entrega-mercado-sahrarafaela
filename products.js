94ea980b11746e09f9ce45722be5b310f2c279bc
sahrarafaela <sararafaela704@gmail.com>
2022-07-15 12:47:31 -0300

    att

--- products.js
+++ products.js
@@ -88,3 +88,71 @@ const products = [
     imageDescription: "",
   },
 ];
+
+
+
+function tags(obj) {
+  
+  let productMain = document.createElement('main')
+  productMain.classList.add("product-main")
+
+  let productCard = document.createElement('li')
+  productCard.classList.add('product')
+
+  let productTitle = document.createElement('h1')
+  productTitle.classList.add('product-title"')
+  productTitle.innerText = obj.title
+
+  let productCategory = document.createElement('h5')
+  productCategory.classList.add('product-category')
+  productCategory.innerText = obj.category
+
+  let productImg = document.createElement('img')
+  productImg.classList.add('product-img')
+  productImg.src = obj.image
+  productImg.alt = obj.imageDescription
+
+  if (obj.image == undefined) {
+    productImg.classList.add('product-img')
+    productImg.src = "./img/products/no-img.svg"
+    productImg.alt = obj.imageDescription
+  }
+
+  let productPrice = document.createElement('strong')
+  productPrice.classList.add('product-price')
+  productPrice.innerText = "R$ " + obj.price
+
+  productMain.append(productTitle, productCategory, productPrice)
+  productCard.append(productImg, productMain)
+
+  return productCard
+}
+
+function constLinst(frutas, bebidas, higiene) {
+  for (let p of products) {
+    if (p.category == "Frutas") {
+      frutas.append(tags(p))
+    } else if (p.category == "Higiene") {
+      bebidas.append(tags(p))
+    } else if (p.category == "Bebidas") {
+      higiene.append(tags(p))
+    }
+  }
+}
+
+function final() {
+  let fruit = document.querySelector(".fruits")
+  let drinks = document.querySelector(".drinks")
+  let hygiene = document.querySelector(".hygiene")
+  let frutinhas = document.createElement('ul')
+  console.log(frutinhas)
+  let lBebidas = document.createElement('ul')
+  let lHigiene = document.createElement('ul')
+  
+  fruit.append(frutinhas)
+  drinks.append(lBebidas)
+  hygiene.append(lHigiene)
+  
+  constLinst(frutinhas, lBebidas, lHigiene)
+}
+final()
